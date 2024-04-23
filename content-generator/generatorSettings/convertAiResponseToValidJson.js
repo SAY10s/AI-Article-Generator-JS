36dc79c1ffd52516497c3e95065faf76ca31586d
SAY10s <nikodemmajzner@gmail.com>
2024-04-23 09:04:05 +0200

    ADD: AI Response to valid JSON (keeping new lines inside content)

---
+++ content-generator/generatorSettings/convertAiResponseToValidJson.js
@@ -0,0 +1,19 @@
+export function convertAiResponseToValidJson(data) {
+  try {
+    let splitData = data.split('"');
+    splitData.forEach((element, index) => {
+      console.log(index + ": " + element);
+    });
+    splitData = splitData.map((element, index) => {
+      if (index % 4 === 3) {
+        return element.replace(/[\n\t\r]/g, "<br>");
+      } else {
+        return element.replace(/[\n\t\r]/g, " ").replace(/\s+/g, "");
+      }
+    });
+    data = splitData.join('"');
+    return data;
+  } catch (err) {
+    console.error("An error occurred:", err);
+  }
+}
